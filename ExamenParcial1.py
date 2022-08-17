fa6d2b855268fba3b97316368b97858d5e914f71
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-08-16 23:33:14 -0600

    Program finished

--- ExamenParcial1.py
+++ ExamenParcial1.py
@@ -1,3 +1,10 @@
+# Universidad del Valle de Guatemala
+# Departamento de Ingeniería
+# Matematica Discreta seccion 10
+# Fabián Estuardo Juárez Tello 21440
+# Catedrático: Mario Castillo
+
+#Crear las variables y listas a utilizar
 dato = ""
 U = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
 CU = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
@@ -6,6 +13,7 @@ AF = []
 B = []
 BF = []
 
+#Metodo que permite el ingreso de los datos para el conjunto A
 def mostrar_ingreso_A(dato):
     while dato != "no" or dato !="No":
         dato = input('Desea ingresar un dato para el conjunto A?\n')
@@ -18,8 +26,8 @@ def mostrar_ingreso_A(dato):
     print("Los elementos que posee el elemento A son: ")
     print(A)
 
+#Metodo que permite el ingreso de los datos para el conjunto B
 def mostrar_ingreso_B(dato2):
-
     while dato2 != "no":
         dato2 = input('\n\nDesea ingresar un dato para el conjunto B?\n')
         if (dato2 =="si" or dato2 == "Si"):
@@ -31,9 +39,12 @@ def mostrar_ingreso_B(dato2):
     print("Los elementos que posee el elemento B son:" )
     print(B)
 
+#Metodo que permite mostrar los elementos para cada uno de los conjuntos en la conversion a binario, siendo
+# 1 que si lo posee y 0 que no lo posee el conjunto a comparacion del conjunto universo
 def mostrar_datos_Finales(A,B):
     print(U)
     print("Los elementos que posee el conjunto final de cada uno son: \n")
+    #Metodo que recorre el conjunto A para la obtencion de los datos para el conjunto
     for element in U:
         if element in A:
             AF.append(1)
@@ -41,6 +52,7 @@ def mostrar_datos_Finales(A,B):
             AF.append(0)
     print("Conjunto A:")
     print(AF)
+    #Metodo que recorre el conjunto B para la obtencion de los datos para el conjunto
     for element in U:
         if element in B:
             BF.append(1)
@@ -49,22 +61,23 @@ def mostrar_datos_Finales(A,B):
     print("Conjunto B:")
     print(BF)
 
+#Metodo que posee la creacion del menu y su muestra 
 def mostrar_menu(opciones):
     print('\nSeleccione una opción:')
     for clave in sorted(opciones):
         print(f' {clave}) {opciones[clave][0]}')
 
-
+#Metodo que leera la opcion ingresada por el usuario y devolvera error si no ha escogido una valida
 def leer_opcion(opciones):
     while (a := input('Opción: ')) not in opciones:
         print('Opción incorrecta, vuelva a intentarlo.')
     return a
 
-
+#Metodo que ejecutara la opcion escogida por el usuario
 def ejecutar_opcion(opcion, opciones):
     opciones[opcion][1]()
 
-
+#Metodo que genera el menu en base a las opciones y las salidas que hay a mostrar
 def generar_menu(opciones, opcion_salida):
     opcion = None
     while opcion != opcion_salida:
@@ -73,7 +86,7 @@ def generar_menu(opciones, opcion_salida):
         ejecutar_opcion(opcion, opciones)
         print()
 
-
+#Metodo que se utilizara para llevar a cabo todos los procesos (sera el metodo main)
 def menu_principal():
     mostrar_ingreso_A(dato)
     mostrar_ingreso_B(dato)
@@ -86,13 +99,13 @@ def menu_principal():
         '5': ('busqueda de un elemento especifico', accion5),
         '6': ('Salir',salir)
     }
-
     generar_menu(opciones, '5')
 
-
+#Metodo que realizara la union de los conjuntos A y B
 def accion1():
     UF = []
     print('\n\t\tHas elegido la Union')
+    #recorrera el conjunto A y B y verificara que la menos uno de los 2 tenga el dato para colocar 1
     for i in range(36):
         if((AF[i]== 1 and BF[i]==1)or (AF[i]== 1 and BF[i]== 0) or (AF[i]== 0 and BF[i]==1)):
             UF.append(1)
@@ -101,10 +114,11 @@ def accion1():
     print("\tConjunto Union de A y B:")
     print (UF)
 
-
+#Metodo que realizara la interseccion de los conjuntos A y B
 def accion2():
     IF = []
     print('\n\t\tHas elegido Intersección')
+    #recorrera el conjunto A y B y verificara que ambos tengan el dato para colocar 1
     for i in range(36):
         if(AF[i]== 1 and BF[i]==1):
             IF.append(1)
@@ -113,11 +127,13 @@ def accion2():
     print("\tConjunto Intersección de A y B:")
     print (IF)
 
+#Metodo que realizara la Diferencia de los conjuntos A y B dependiendo cual escoja el usuario
 def accion3():
     DF = []
     print('\n\t\tHas elegido Diferencia')
     conjunto = input("En que conjunto desea aplicar la diferencia?")
     if(conjunto == "A" or conjunto == "a"):
+    #recorrera el conjunto A y B y verificara que solo A tenga el dato y B no
         for i in range(36):
             if(AF[i]== 1 and BF[i]==0):
                 DF.append(1)
@@ -126,6 +142,7 @@ def accion3():
         print("\tDiferencia de A hacia B:")
         print (DF)
     elif(conjunto == "B" or conjunto == "b"):
+    #recorrera el conjunto A y B y verificara que solo B tenga el dato y A no
         for i in range(36):
             if(AF[i]== 0 and BF[i]==1):
                 DF.append(1)
@@ -134,12 +151,13 @@ def accion3():
         print("\tDiferencia de B hacia A:")
         print (DF)
 
-
+#Metodo que realizara el complemento de los conjuntos A y B, segun la eleccion del usuario
 def accion4():
     CF = []
     print('\n\t\tHas elegido Complemento')
     conjunto = input("En que conjunto desea aplicar el complemento?")
     if(conjunto == "A" or conjunto == "a"):
+    #recorrera el conjunto A y verificara que datos le hacen falta a A que tiene el conjunto U
         for i in range(36):
             if(AF[i]==0 and CU[i]==1):
                 CF.append(1)
@@ -148,6 +166,7 @@ def accion4():
         print("\tEl complemento en A es:")
         print (CF)
     elif(conjunto == "B" or conjunto == "b"):
+    #recorrera el conjunto B y verificara que datos le hacen falta a B que tiene el conjunto U
         for i in range(36):
             if(BF[i]== 0 and CU[i]==1):
                 CF.append(1)
@@ -156,30 +175,32 @@ def accion4():
         print("\tEl complemento en B es:")
         print (CF)
     
-
+#Metodo que realizara la busqueda de un elemento en especifico de cualquiera de los 2 conjuntos, sera eleccion del usuario
 def accion5():
     D = input('Ingrese el dato que desea buscar en el conjunto:')
     conjunto = input("En que conjunto desea buscar el dato?")
     if(conjunto == "A" or conjunto == "a"):
+    #recorrera el conjunto A y verificara que el dato que se busca se encuentra
         for element in A:
             if(element in D):
                 bandera = True
                 break
         if (bandera == True):
-            print(f"El elemento {D} si se ha encontrado en el conjunto {conjunto}!")
+            print(f"El elemento {D} si se ha encontrado en el conjunto {conjunto}!")# si se encuentra imprimira esto
         else:
-            print(f"El elemento {D} no se ha encontrado en el conjunto {conjunto}!")
+            print(f"El elemento {D} no se ha encontrado en el conjunto {conjunto}!")# si no se encuentra imprimira esto
     elif(conjunto == "B" or conjunto == "b"):
+    #recorrera el conjunto B y verificara que el dato que se busca se encuentra
         for element in B:
             if(element in D):
                 bandera = True
                 break
         if (bandera == True):
-            print(f"El elemento {D} si se ha encontrado en el conjunto {conjunto}!")
+            print(f"El elemento {D} si se ha encontrado en el conjunto {conjunto}!")# si se encuentra imprimira esto
         else:
-            print(f"El elemento {D} no se ha encontrado en el conjunto {conjunto}!")
+            print(f"El elemento {D} no se ha encontrado en el conjunto {conjunto}!")# si no se encuentra imprimira esto
 
-
+#Metodo que cerrara el programa si el usuario lo elije
 def salir():
     print('Saliendo')
     exit()
